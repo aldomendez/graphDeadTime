aa7e954e9fd95a3cc5dd2eed49a0b3ea99cd1080
Aldo Mendez <aldomendez86@gmail.com>
2015-08-23 18:00:29 -0500

    Initial commit

---
+++ coffee/barChar_d3/barChar_d3.js
@@ -0,0 +1,26 @@
+(function() {
+  var bO, bW, chartdata, h, w;
+
+  chartdata = [40, 60, 80, 100, 70, 120, 100, 60, 70, 150, 120, 140];
+
+  h = 222;
+
+  w = 720;
+
+  bW = 40;
+
+  bO = 20;
+
+  d3.select('#bar-chart').append('svg').attr('width', w).attr('height', h).style('background', '#dff0d8').selectAll('rect').data(chartdata).enter().append().style({
+    'fill': '#3c763d',
+    'stroke': '#d6e9c6',
+    'stroke-width': '5'
+  }).attr('width', bW).attr('height', function(data) {
+    return data;
+  }).attr('x', function(data, i) {
+    return i * (bW + bO);
+  }).attr('y', function(data) {
+    return h - data;
+  });
+
+}).call(this);
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Bar Graph</title>
+</head>
+<body>
+<div class="someClass">
+  <h2>Create a bar chart wuth D3 javascript</h2>
+  <div id="bar-chart"></div>
+</div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.js" charset="utf-8"></script>
+  <script src="js/barChard3.js"></script>
+</body>
+</html>
